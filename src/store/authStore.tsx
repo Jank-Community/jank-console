437e0c9711ad9e2b85a2e4ecf22c8eb228adb7d4
xinghe98 <lcg9899@gmail.com>
2025-05-19 22:24:33 +0800

    style: 格式化代码

--- src/store/authStore.tsx
+++ src/store/authStore.tsx
@@ -1,47 +1,57 @@
 import { create } from 'zustand'
-import api from '../axiosInstance'; // 引入 Axios 实例
-import {parseQuery} from '@/utils'
 
-export const useAuthStore = create((set) => ({
-    name: '',
-    permissions: [],
-    token: '',
-    refreshToken: '',
-    publicKey: '',
-    isLoading: false,
-    error: null,
-    login: async (username: string, password: string) => {
-        set({isLoading: true, error: null});
-        try {
-            const data: any = await api.post('/login?' + parseQuery({username, password}));
-            if (data.code == 200) {
-                set({
-                    name: data.data.nickname,
-                    permissions: data.data.permissions,
-                    token: data.data.token,
-                    refreshToken: data.data.refreshToken,
-                    isLoading: false
-                });
-            } else {
-                set({
-                    isLoading: false
-                })
-            }
-            return data
-        } catch (error: any) {
-            set({error: error.response?.data?.msg || 'Login failed', isLoading: false});
-        }
-    },
-    getPublicKey: async () => {
-        const data: any = await api.get('/v1/publicKey');
+// 引入 Axios 实例
+import { parseQuery } from '@/utils'
+
+import api from '../axiosInstance'
+
+interface authStore {
+  name: string
+  permissions: any[]
+  token: string | null
+  refreshToken: string
+  isLoading: boolean
+  error: string | null
+  login: (username: string, password: string) => Promise<any>
+  logout: () => void
+}
+
+export const useAuthStore = create<authStore>((set) => ({
+  name: '',
+  permissions: [],
+  token: '',
+  refreshToken: '',
+  isLoading: false,
+  error: null,
+  login: async (username: string, password: string) => {
+    set({ isLoading: true, error: null })
+    try {
+      const data: any = await api.post('/account/loginAccount')
+      if (data.code == 200) {
         set({
-            publicKey: data.data
+          name: data.data.nickname,
+          permissions: data.data.permissions,
+          token: data.data.token,
+          refreshToken: data.data.refreshToken,
+          isLoading: false,
         })
-    },
-    logout: () => {
-        set({token: null, refreshToken: '', user: null})
-        localStorage.removeItem("token")
-        localStorage.removeItem("refreshToken")
-        window.location.href = '/login'
-    },
-}));
+      } else {
+        set({
+          isLoading: false,
+        })
+      }
+      return data
+    } catch (error: any) {
+      set({
+        error: error.response?.data?.msg || 'Login failed',
+        isLoading: false,
+      })
+    }
+  },
+  logout: () => {
+    set({ token: null, refreshToken: '' })
+    localStorage.removeItem('token')
+    localStorage.removeItem('refreshToken')
+    window.location.href = '/login'
+  },
+}))
