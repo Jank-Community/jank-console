6bfcf7804be7c4d4d62f4f2bfb198d9b077a5b76
xinghe98 <lcg9899@gmail.com>
2025-05-22 13:13:18 +0800

    style: 格式化代码

--- src/components/nav-system.tsx
+++ src/components/nav-system.tsx
@@ -1,44 +1,51 @@
-"use client"
+'use client'
 
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
+
+import { LucideIcon } from 'lucide-react'
+import { useTranslation } from 'react-i18next'
+
+import { Badge } from '@/components/ui/badge'
 import {
-    SidebarGroup,
-    SidebarGroupLabel,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-} from "@/components/ui/sidebar"
-import * as React from "react";
-import {useTranslation} from "react-i18next";
-import {Badge} from "@/components/ui/badge";
-import {LucideIcon} from "lucide-react";
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
 
 export function NavSystem({
-                                projects, label
-                            }: {
-    projects: {
-        name: any
-        url: string
-        icon: LucideIcon
-    }[]
+  projects,
+  label,
+}: {
+  projects: {
+    name: any
+    url: string
+    icon: LucideIcon
+  }[]
+  label: string
 }) {
-    const {t} = useTranslation();
-    return (
-        <SidebarGroup>
-            <SidebarGroupLabel>{t(label)}</SidebarGroupLabel>
-            <SidebarMenu>
-                {projects.map((item) => (
-                    <SidebarMenuItem key={item.name}>
-                        <SidebarMenuButton asChild tooltip={t(item.name)}>
-                            <NavLink to={item.url}>
-                                <item.icon />
-                                <span>{t(item.name)}</span>
-                                {item.name == 'monitor' && <Badge className={'font-medium'} variant="secondary">Beta</Badge>}
-                            </NavLink>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
-                ))}
-            </SidebarMenu>
-        </SidebarGroup>
-    )
+  const { t } = useTranslation()
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>{t(label)}</SidebarGroupLabel>
+      <SidebarMenu>
+        {projects.map((item) => (
+          <SidebarMenuItem key={item.name}>
+            <SidebarMenuButton asChild tooltip={t(item.name)}>
+              <NavLink to={item.url}>
+                <item.icon />
+                <span>{t(item.name)}</span>
+                {item.name == 'monitor' && (
+                  <Badge className={'font-medium'} variant="secondary">
+                    Beta
+                  </Badge>
+                )}
+              </NavLink>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  )
 }
